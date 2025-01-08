3a7493cd64ffdd15c8439070ef7f376d99294ff0
leejung <dlwjdwns424@naver.com>
2025-01-08 22:51:19 +0900

    "adminver2"

--- reactboot/src/main/java/com/example/firstproject/admin/admincontroller.java
+++ reactboot/src/main/java/com/example/firstproject/admin/admincontroller.java
@@ -2,7 +2,9 @@ package com.example.firstproject.admin;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +29,7 @@ public class admincontroller {
 		
 		return null;
 	}
-	
+	//=================================멤버페이지관리========================================
 	@GetMapping("/membermanage")
 	public ResponseEntity memberpage(@RequestParam(defaultValue = "1") int page) {
 		
@@ -38,24 +40,62 @@ public class admincontroller {
 		return ResponseEntity.ok(memberlist);
 		
 	}
-	
+	//=================================게시판페이지관리========================================
 	@GetMapping("/noticemanage")
-	public ResponseEntity noticemanage(@RequestParam(defaultValue="1") int page) {
-		Page<NoticeDto> memberlist=adminservice.allnoticeget(page);
+	public ResponseEntity noticemanage(@RequestParam(defaultValue="1") int page,
+			@RequestParam(required = false) String option,
+			@RequestParam(required =false) String searchtext
+			) {
+		System.out.println("노티스컨트롤페이지:"+page);
+		System.out.println("노티스컨트롤서치:"+searchtext);
 		
-		 return ResponseEntity.ok(memberlist);
+		if(searchtext==null) {
+			Page<NoticeDto> memberlist=adminservice.allnoticeget(page);
+			return ResponseEntity.ok(memberlist);
+		}else {
+			Page<NoticeDto> memberlist=adminservice.searchnotice(page,option,searchtext);
+			
+			return ResponseEntity.ok(memberlist);
+		}
+		
+		
+		
+		 
 	}
+	
+	//삭제 
+	@DeleteMapping("/notice/{noticeid}/delete")
+	public ResponseEntity noticedelete(@PathVariable long noticeid) throws IllegalAccessException {
+		adminservice.deletenotice(noticeid);
+		
+		return ResponseEntity.ok("게시글삭제성공");
+	}
+	
+	//=================================댓글페이지관리========================================
 	@GetMapping("/commentmanage")
 	public ResponseEntity commentmanage(@RequestParam(defaultValue="1") int page) {
 		//Page<NoticeDto> memberlist=adminservice.allCommentrget(page);
 		
 		 return null;
 	}
+	
+	//=================================채팅방페이지관리========================================
 	@GetMapping("/chatroommanage")
 	public ResponseEntity chtroommanage(@RequestParam(defaultValue="1") int page) {
 		Page<roomlistresponseDto> memberlist=adminservice.allRoomget(page);
 		
 		 return ResponseEntity.ok(memberlist);
 	}
+	
+	
+	//=================================멤버페이지관리========================================
+	
+	@DeleteMapping("/member/{userid}/delete")
+	public ResponseEntity deletemember(@PathVariable("userid") Long userid) throws IllegalAccessException {
+		System.out.println("유저삭제"+userid);
+		String message=adminservice.memberdelete(userid);
+		return ResponseEntity.ok(message);
+	}
+	
 	
 }
